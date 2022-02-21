336edea7a18a70c596ee55a6e19eea3dfaed055c
Евгений <Skakovsku@yandex.ru>
2022-02-21 17:30:59 +0300

    Created REVIEW

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,14 +3,12 @@ from rest_framework.response import Response
 from reviews.models import Comment, Review, Title
 from .serializers import CommentSerializer, ReviewSerializer
 from .permissions import IsAuthorOrAdministrationOrReadOnly
-#from rest_framework.permissions import AllowAny
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = (IsAuthorOrAdministrationOrReadOnly,)
-    #permission_classes = (AllowAny,)
 
     def list(self, request, *arg, **kwargs):
         if Title.objects.filter(pk=kwargs['id']).count() == 0:
@@ -33,6 +31,30 @@ class ReviewViewSet(viewsets.ModelViewSet):
             error = {"detail": "Произведение не найдено."}
             return Response(error, status=status.HTTP_404_NOT_FOUND)
         return super(ReviewViewSet, self).create(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        if Title.objects.filter(pk=kwargs['id']).count() != 0:
+            title = Title.objects.get(pk=kwargs['id'])
+            if Review.objects.filter(title=title).count() != 0:
+                queryset = Review.objects.filter(title=title)
+                for review in queryset:
+                    if str(review.id) == kwargs['pk']:
+                        serialiser = ReviewSerializer(review)
+                        return Response(serialiser.data)
+        error = {"detail": "Произведение или отзыв не найден"}
+        return Response(error, status=status.HTTP_404_NOT_FOUND)
+
+    def update(self, request, *args, **kwargs):
+        if Title.objects.filter(pk=kwargs['id']).count() == 0:
+            error = {"detail": "Произведение не найдено"}
+            return Response(error, status=status.HTTP_404_NOT_FOUND)
+        return super(CommentViewSet, self).update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        if Title.objects.filter(pk=kwargs['id']).count() == 0:
+            error = {"detail": "Произведение или отзыв не найден"}
+            return Response(error, status=status.HTTP_404_NOT_FOUND)
+        return super(CommentViewSet, self).destroy(request, *args, **kwargs)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
